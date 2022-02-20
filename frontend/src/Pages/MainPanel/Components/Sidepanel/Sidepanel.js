5b143cb68d3ca060a7c5b6b33cb7e72318ff3c51
Bartosz Latosek <latosek.bar@gmail.com>
2022-02-20 18:50:01 +0100

    further view improvement, now works on mobile devices

--- frontend/src/Pages/MainPanel/Components/Sidepanel/Sidepanel.js
+++ frontend/src/Pages/MainPanel/Components/Sidepanel/Sidepanel.js
@@ -12,36 +12,45 @@ import Friendlist from "../Friendlist/Friendlist";
 const Sidepanel = () => {
   const [state, setState] = useState(0);
 
+  const updateState = (val) => {
+    if (state === val){
+      setState(0)
+    }
+    else {
+      setState(val)
+    }
+  }
+
   return (
     <>
       <div className="Sidepanel_main">
         <div className="Sidepanel_navbar">
           <div
-            className={`Sidepanel_navbar-button ${state === 0 && "selected"}`}
-            onClick={() => setState(0)}
+            className={`Sidepanel_navbar-button ${state === 1 && "selected"}`}
+            onClick={() => updateState(1)}
           >
             <BsFillPersonLinesFill />
           </div>
           <div
-            className={`Sidepanel_navbar-button ${state === 1 && "selected"}`}
-            onClick={() => setState(1)}
+            className={`Sidepanel_navbar-button ${state === 2 && "selected"}`}
+            onClick={() => updateState(2)}
           >
             <BsFillPersonPlusFill />
           </div>
           <div
-            className={`Sidepanel_navbar-button ${state === 2 && "selected"}`}
-            onClick={() => setState(2)}
+            className={`Sidepanel_navbar-button ${state === 3 && "selected"}`}
+            onClick={() => updateState(3)}
           >
             <BsFillEnvelopeFill />
           </div>
           <div className="Sidepanel_navbar-footer" />
         </div>
-        <div className="default-block">
-          {state === 0 && <Friendlist />}
-          {state === 1 && <AddFriend />}
-          {state === 2 && <FriendRequests />}
-        </div>
       </div>
+        {state !== 0 && <div className="default-block">
+          {state === 1 && <Friendlist />}
+          {state === 2 && <AddFriend />}
+          {state === 3 && <FriendRequests />}
+      </div>}
     </>
   );
 };
